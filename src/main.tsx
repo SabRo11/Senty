03488222665355bef283463fbc89d2614e6b0af2
SabRo11 <sabrina.rosato@havas.com>
2026-09-09 10:28:36 +0200

    fix: load strict v0.7.2 alignment overrides

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './v05.css'
 import './v06.css'
 import './v07.css'
 import './v071.css'
+import './v072.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
