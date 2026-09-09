3f5fdbd6ae68468fae7a5c25839e064bb8e01ea4
SabRo11 <sabrina.rosato@havas.com>
2026-09-09 10:18:44 +0200

    feat: load SENTY-LP v0.7.1 styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css'
 import './v05.css'
 import './v06.css'
 import './v07.css'
+import './v071.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
